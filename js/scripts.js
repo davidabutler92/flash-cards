2ac1f2c544091edd283c6ea4602c6ed0726c8c71
Tyler Bates <Fondlytyler@gmail.com>
2020-03-24 14:32:09 -0700

    make def1 hidden initially and make def1 appear when button pressed!!!

--- js/scripts.js
+++ js/scripts.js
@@ -1,96 +1,103 @@
 $(document).ready(function() {
-  $("button#".click(function() {
-    $("body").removeClass();
-    
+  
+  $("button#javascript").click(function() {
+    $("#card1").fadeToggle();
+    $("#def1").fadeToggle();
   });
   
   $("button#operators").click(function() {
     $("body").removeClass();
-    $("body").addClass("yellow-background")
+    $("#card2").addClass("card2");
   });
 
   $("button#variables").click(function() {
     $("body").removeClass();
-    $("body").addClass("red-background");
+    $("body").addClass("card3");
   });
   
   $("button#varnc").click(function() {
     $("body").removeClass();
-    $("body").addClass("yellow-background")
+    $("body").addClass("card3")
   });
 
   $("button#functions").click(function() {
     $("body").removeClass();
-    $("body").addClass("red-background");
+    $("body").addClass("card4");
   });
   
   $("button#methods").click(function() {
     $("body").removeClass();
-    $("body").addClass("yellow-background")
+    $("body").addClass("card5")
   });
   
   $("button#arguments").click(function() {
     $("body").removeClass();
-    $("body").addClass("red-background");
+    $("body").addClass("card6");
   });
 
   $("button#parameters").click(function() {
     $("body").removeClass();
-    $("body").addClass("yellow-background")
+    $("body").addClass("card7")
   });
 
   $("button#return").click(function() {
     $("body").removeClass();
-    $("body").addClass("red-background");
+    $("body").addClass("card8");
   });
   
   $("button#chaining").click(function() {
     $("body").removeClass();
-    $("body").addClass("yellow-background")
+    $("body").addClass("card9")
   });
 
   $("button#strings").click(function() {
     $("body").removeClass();
-    $("body").addClass("red-background");
+    $("body").addClass("card10");
   });
   
   $("button#booleans").click(function() {
     $("body").removeClass();
-    $("body").addClass("yellow-background")
+    $("body").addClass("card11")
   });
   
   $("button#undefined").click(function() {
     $("body").removeClass();
-    $("body").addClass("red-background");
+    $("body").addClass("card12");
   });
 
   $("button#nan").click(function() {
     $("body").removeClass();
-    $("body").addClass("yellow-background")
+    $("body").addClass("card13")
   });
 
   $("button#escape").click(function() {
     $("body").removeClass();
-    $("body").addClass("red-background");
+    $("body").addClass("card14");
   });
   
   $("button#alert").click(function() {
     $("body").removeClass();
-    $("body").addClass("yellow-background")
+    $("body").addClass("card15")
   });
 
   $("button#comments").click(function() {
     $("body").removeClass();
-    $("body").addClass("red-background");
+    $("body").addClass("card16");
   });
   
   $("button#jQuery").click(function() {
     $("body").removeClass();
-    $("body").addClass("yellow-background")
+    $("body").addClass("card17")
   });
   
   $("button#attributes").click(function() {
     $("body").removeClass();
-    $("body").addClass("red-background");
+    $("body").addClass("card18");
   });
+  
+  $("button#attributes").click(function() {
+    $("body").removeClass();
+    $("body").addClass("card19");
+  });
+  
 });
