1b053231dee824f836ed4382c7cdfb3455e873ef
Tyler Bates <Fondlytyler@gmail.com>
2020-03-24 13:58:59 -0700

    match terms with our buttons on html and Scripts.js

---
+++ js/scripts.js
@@ -0,0 +1,96 @@
+$(document).ready(function() {
+  $("button#".click(function() {
+    $("body").removeClass();
+    
+  });
+  
+  $("button#operators").click(function() {
+    $("body").removeClass();
+    $("body").addClass("yellow-background")
+  });
+
+  $("button#variables").click(function() {
+    $("body").removeClass();
+    $("body").addClass("red-background");
+  });
+  
+  $("button#varnc").click(function() {
+    $("body").removeClass();
+    $("body").addClass("yellow-background")
+  });
+
+  $("button#functions").click(function() {
+    $("body").removeClass();
+    $("body").addClass("red-background");
+  });
+  
+  $("button#methods").click(function() {
+    $("body").removeClass();
+    $("body").addClass("yellow-background")
+  });
+  
+  $("button#arguments").click(function() {
+    $("body").removeClass();
+    $("body").addClass("red-background");
+  });
+
+  $("button#parameters").click(function() {
+    $("body").removeClass();
+    $("body").addClass("yellow-background")
+  });
+
+  $("button#return").click(function() {
+    $("body").removeClass();
+    $("body").addClass("red-background");
+  });
+  
+  $("button#chaining").click(function() {
+    $("body").removeClass();
+    $("body").addClass("yellow-background")
+  });
+
+  $("button#strings").click(function() {
+    $("body").removeClass();
+    $("body").addClass("red-background");
+  });
+  
+  $("button#booleans").click(function() {
+    $("body").removeClass();
+    $("body").addClass("yellow-background")
+  });
+  
+  $("button#undefined").click(function() {
+    $("body").removeClass();
+    $("body").addClass("red-background");
+  });
+
+  $("button#nan").click(function() {
+    $("body").removeClass();
+    $("body").addClass("yellow-background")
+  });
+
+  $("button#escape").click(function() {
+    $("body").removeClass();
+    $("body").addClass("red-background");
+  });
+  
+  $("button#alert").click(function() {
+    $("body").removeClass();
+    $("body").addClass("yellow-background")
+  });
+
+  $("button#comments").click(function() {
+    $("body").removeClass();
+    $("body").addClass("red-background");
+  });
+  
+  $("button#jQuery").click(function() {
+    $("body").removeClass();
+    $("body").addClass("yellow-background")
+  });
+  
+  $("button#attributes").click(function() {
+    $("body").removeClass();
+    $("body").addClass("red-background");
+  });
+});
